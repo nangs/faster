21bfc3132a0021d6800f263c6ca1756b7414db5a
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-09-10 21:52:41 -0400

    Format strings used for snippets so the pre tag can format them appropriately

--- src/app/constants/languages.js
+++ src/app/constants/languages.js
@@ -1,15 +1,13 @@
 'use strict';
 
 var javaSnippets = [
-    'for(int i = 0; i < length; i++){ ` total += i; ` }',
-    'if(variable > constant){ ` variable % 12 ` }else{ ` variable++ ` }',
-    'public static void main (String args[]){ ` ` }',
-    'public class ClassName(){ ` ` }',
-    'public static int sum(int n, int k){ ` return n + k ` }'
+    'for(int i = 0; i < length; i++){\n\ttotal += i;\n}',
+    'if(variable > constant){\n\tvariable % 12\n}else{\n\tvariable++\n}',
+    'public static void main (String args[]){\n\n}',
+    'public static int sum(int n, int k){\n\treturn n + k\n}'
 ];
 
 var javascriptSnippets = [
-    'document.querySelector(\'.start\').addEventListener(\'click\', start);',
     'document.querySelector(\'.start\').addEventListener(\'click\', start);'
 ];
 
