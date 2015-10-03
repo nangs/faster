9346b5478fb68abc683f7ba50647951abd8d8a5b
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-10-02 23:52:51 -0400

    Updated icons.

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,11 @@
 'use strict';
 
+require('./styles/assets/icon.png');
+require('./styles/assets/apple-icon-57x57.png');
+require('./styles/assets/apple-icon-72x72.png');
+require('./styles/assets/apple-icon-114x114.png');
+require('./styles/assets/apple-icon-144x144.png');
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
