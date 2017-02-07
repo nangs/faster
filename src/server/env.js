5f3b350ef3720c416cf4b5786941595f1d1b82f3
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-02-07 12:22:14 -0500

    Update the env files.

--- src/server/env.js
+++ src/server/env.js
@@ -5,7 +5,8 @@ const {
     HOST, PORT,
     DEV_HOST, DEV_PORT,
     API_HOST, API_PORT,
-    MONDO_URI
+    MONDO_URI,
+    MAILGUN_API_KEY, MAILGUN_DOMAIN
 } = process.env;
 
 export const isBrowser = process.browser;
@@ -18,5 +19,5 @@ export const devPort = DEV_PORT || 3001;
 export const apiHost = API_HOST || DEFAULT_HOST;
 export const apiPort = API_PORT || 3002;
 export const mongoUri = MONDO_URI || 'mongodb://localhost/imap';
-export const mailgunApiKey = 'a';
-export const mailgunDomain = 'a';
+export const mailgunApiKey = MAILGUN_API_KEY;
+export const mailgunDomain = MAILGUN_DOMAIN;
