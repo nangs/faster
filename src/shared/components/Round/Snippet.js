ea06d8a19fd877e689df8b96020da31a29692bcf
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-02-06 22:55:55 -0500

    Highlight the active character in the snippet.

--- src/shared/components/Round/Snippet.js
+++ src/shared/components/Round/Snippet.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Selectors } from './../../state';
 
 const mapStateToProps = (state) => ({
-    snippet: state.atom.snippet,
+    snippet: Selectors.getFormattedSnippet(state),
     typos: state.atom.typos
 });
 
 @connect(mapStateToProps)
 export default class Snippet extends Component {
     render() {
-        const { snippet, typos } = this.props;
-        const snippetMarkup = snippet
-            .split('')
-            .map((character, index) => (
-                <span key={index} className={typos[index]}>{character}</span>
-            ));
+        const snippet = this.props.snippet.map((character, i) => (
+            <span key={i} className={character.className}>{character.symbol}</span>
+        ));
         return (
             <div className="snippet">
-                <pre className="center">{snippetMarkup}</pre>
+                <pre className="center">{snippet}</pre>
             </div>
         )
     }
---
+++ src/shared/state/selectors/getFormattedSnippet.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+import indexSelector from './getIndex';
+
+const snippetSelector = state => state.atom.snippet;
+const typosSelector = state => state.atom.typos;
+
+export default createSelector(
+    [snippetSelector, typosSelector, indexSelector],
+    (snippet, typos, index) => snippet
+        .split('')
+        .map((symbol, i) => ({
+            symbol: symbol,
+            className: [
+                typos[i],
+                i === index ? 'active' : 'passive',
+                symbol === '\n' ? 'carriage-return' : ''
+            ].join(' ')
+        }))
+);
