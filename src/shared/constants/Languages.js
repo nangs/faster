55b0e63c855663fa9252211418ed10710ac95fe7
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-02-10 13:38:34 -0500

    Update snippets

--- src/shared/constants/Languages.js
+++ src/shared/constants/Languages.js
@@ -1,12 +1,12 @@
 const javaSnippets = [
     'for(int i = 0; i < length; i++){\n\ttotal += i;\n}',
-    'if(constiable > constant){\n\tconstiable % 12\n}else{\n\tconstiable++\n}',
+    'if(speed > constant){\n\speed % 12\n}else{\n\speed++\n}',
     'public static void main (String args[]){\n\n}',
     'public static int sum(int n, int k){\n\treturn n + k\n}'
 ];
 
 const javascriptSnippets = [
-    'document.querySelector(\'.start\').addEventListener(\'click\', start);'
+    'var main = function() {\n\tdocument\n\t.querySelector(\'.start\')\n\t.addEventListener(\'click\', start);\n}'
 ];
 
 const csharpSnippets = [
